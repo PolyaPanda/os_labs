facfcb6b902f7a6247fbbd083f1036e1b6a9c70d
PolyaPanda <121058905+PolyaPanda@users.noreply.github.com>
2023-11-12 23:25:58 +0300

    lab1_task2_both

---
+++ task2_linuha.c
@@ -0,0 +1,59 @@
+﻿#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void signalHandler(int);
+void* printRandNum(void*);
+void* readFromConsole(void*);
+
+int main(int argc, char** argv) {
+	pthread_t thread1, thread2;
+	int ret1, ret2;
+
+	signal(SIGINT, signalHandler);
+	signal(SIGTERM, signalHandler);
+	ret1 = pthread_create(&thread1, NULL, printRandNum, NULL);
+	ret2 = pthread_create(&thread2, NULL, readFromConsole, NULL);
+	while (1);
+	return 0;
+}
+
+void* printRandNum(void*) {
+	while (1) {
+		printf("%f\n", (float)rand() / ((float)RAND_MAX / 1));
+		sleep(3);
+	}
+
+	return 0;
+}
+
+void* readFromConsole(void*) {
+	while (1) {
+		char str[100];
+		scanf("%s", str);
+		for (int i = strlen(str) - 1; i >= 0; --i) {
+			printf("%c", str[i]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+void signalHandler(int signalno) {
+	switch (signalno) {
+	case SIGINT:
+		printf("\nSIGINT %d\n", signalno);
+		break;
+	case SIGTERM:
+		printf("\nSIGTERM %d\n", signalno);
+		break;
+	default:
+		printf("\nAnother signal %d\n", signalno);
+		break;
+	}
+	exit(0);
+}
---
+++ task2_winda.cpp
@@ -0,0 +1,78 @@
+﻿#include <thread>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <csignal>
+#include <vector>
+#include <atomic>
+#include <mutex>
+
+std::vector<std::thread> threads;
+std::atomic<bool> stopFlag = false;
+std::mutex coutMutex;
+
+void PrintNumbersWithInterval()
+{
+    while (!stopFlag)
+    {
+        coutMutex.lock();
+        std::cout << static_cast<float>(std::rand()) / RAND_MAX << '\n';
+        coutMutex.unlock();
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    std::cout << "PrintNumbersWithInterval thread finished.\n";
+}
+
+void PrintReversedString(std::string& string)
+{
+    std::reverse(string.begin(), string.end());
+    std::lock_guard<std::mutex> lock(coutMutex);
+    std::cout << string << '\n';
+}
+
+void ReadInput()
+{
+    while (!stopFlag)
+    {
+        std::string input;
+        std::getline(std::cin, input);
+        if (!input.empty())
+        {
+            PrintReversedString(input);
+        }
+    }
+    std::lock_guard<std::mutex> lock(coutMutex);
+    std::cout
+        << "ReadInput thread finished.\n";
+}
+
+void SignalHandler(int signal)
+{
+    if (signal == SIGINT)
+    {
+        std::lock_guard<std::mutex> lock(coutMutex);
+        std::cout << "Signal received. Exiting...\n";
+        stopFlag = true;
+    }
+}
+
+int main()
+{
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    std::signal(SIGINT, SignalHandler);
+    threads.emplace_back(PrintNumbersWithInterval);
+    threads.emplace_back(ReadInput);
+
+    for (auto& thread : threads)
+    {
+        if (thread.joinable())
+        {
+            thread.join();
+        }
+    }
+    std::cout << "Program finished\n";
+    return 0;
+}
