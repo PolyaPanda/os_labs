359c8433ac007039dbcc90367132d166ec8000eb
PolyaPanda <121058905+PolyaPanda@users.noreply.github.com>
2023-11-20 11:50:02 +0300

    Update createProcess_linuha.c

--- lab1_task1/lin/createProcess_linuha.c
+++ lab1_task1/lin/createProcess_linuha.c
@@ -1,24 +1,43 @@
+#include <unistd.h>
 #include <stdio.h>
-#include <signal.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <signal.h>
 
-void sigHandler(int signal)
-{
-    switch (signal) {
-        case SIGINT:
-            printf("\nSIGINT caught.\n");
-            break;
-        case SIGTERM:
-            printf("\nSIGTERM caught.\n");
-            break;
-    }
-
-    exit(0);
-}
+void signHandler(int signal)
+{}
 
 int main()
 {
-    signal(SIGINT, sigHandler);
-    signal(SIGTERM, sigHandler);
-    while(1);
+	pid_t cpid, w;
+	int status;
+
+	cpid = fork();
+	if (cpid == -1) {
+		perror("fork\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (cpid == 0) {
+		sleep(7);
+		return 1;
+	}
+	else {
+		signal(SIGINT, signHandler);
+		signal(SIGTERM, signHandler);
+		w = wait(&status);
+		if (w == -1) {
+			perror("waitpid\n");
+			exit(EXIT_FAILURE);
+		}
+
+		if (WIFEXITED(status))
+			printf("\nNormal exit with status %d\n", WEXITSTATUS(status));
+		else if (WIFSIGNALED(wstatus))
+			printf("\nKilled by signal %d\n", WTERMSIG(status));
+		else if (WIFSTOPPED(status))
+			printf("\nStopped by signal %d\n", WSTOPSIG(status));
+		else if (WIFCONTINUED(status))
+			printf("\nContinued\n");
+	}
 }
