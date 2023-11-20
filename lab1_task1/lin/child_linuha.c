a28881d90049d7df8c13342cf9fa0457ddcef8bf
PolyaPanda <121058905+PolyaPanda@users.noreply.github.com>
2023-11-20 11:50:33 +0300

    Update child_linuha.c

--- lab1_task1/lin/child_linuha.c
+++ lab1_task1/lin/child_linuha.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <signal.h>
+#include <stdlib.h>
 
-void sigHandler() {
+
+void sigHandler(int signalno) {
+    switch (signalno) {
+    case SIGINT:
+        printf("\nSIGINT %d\n", signalno);
+        printf("terminated by ctrl+c");
+        break;
+    case SIGTERM:
+        printf("\nSIGTERM %d\n", signalno);
+        printf("exited by terminated signal");
+        break;
+    default:
+        printf("\nAnother signal\n");
+        break;
+    }
+    exit(0);
 }
 
-int main() {
-    pid_t child_pid;
-    int status;
-
-    child_pid = fork();
-
-    if (child_pid == 0) {
-        while(1);
-        
-    } else if (child_pid > 0) {
-        signal(SIGINT, sigHandler);
-        wait(&status);
-
-        if (WIFEXITED(status)) 
-            printf("\nChild process exited with code: %d\n", WEXITSTATUS(status));
-        else if (WIFSIGNALED(status))
-	    printf("\nChild process stopped by signal %d\n", WTERMSIG(status));
-
-    } else {
-        perror("fork");
-        exit(1);
-    }
-
-    return 0;
+int main(int argc, char** argv) {
+    signal(SIGINT, sigHandler);
+    signal(SIGTERM, sigHandler);
+    while (1);
 }
